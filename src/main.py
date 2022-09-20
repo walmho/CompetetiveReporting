0e5412eef14511a1c869fdcfe7db45932ca06659
walmho <jones.ryan.t.01@gmail.com>
2022-09-20 15:27:21 -0600

    testing scraper to make sure it works, finalizing push to master

--- src/main.py
+++ src/main.py
@@ -1,9 +1,4 @@
 from dataRetrieval.scrapeData import get_page
 
 if __name__ == "__main__":
-    status, houseList = get_page("Hillsboro", "Oregon", "US")
-    print(status)
-    print("\n\n\n")
-    print(len(houseList))
-    print(houseList)
-
+    status, houseList = get_page("Hillsboro", "Oregon", "US", debug=False)
