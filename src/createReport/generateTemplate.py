a931b0a955e89c940970b07094dcc26c667d8f88
Ryan Jones <jones.ryan.t.01@gmail.com>
2022-09-27 14:09:16 -0700

    learning how Jinja works and how to create a template with a txt file

--- src/createReport/generateTemplate.py
+++ src/createReport/generateTemplate.py
@@ -1,1 +1,8 @@
-# Use Jinja2 to create a basic template based on project need
+from jinja2 import Environment, FileSystemLoader
+
+def createBase(txtFile):
+    loader=FileSystemLoader("templates/")
+    environment = Environment(loader)
+    template = environment.get_template(txtFile)
+    template.render()
+    return template
--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from dataRetrieval.scrapeData import get_page
+from createReport.generateTemplate import createBase
 
 if __name__ == "__main__":
-    status, houseList = get_page("Hillsboro", "Oregon", "US", debug=False)
+    #status, houseList = get_page("Hillsboro", "Oregon", "US", debug=False)
+    basic = createBase("basics.txt")
+    print(basic)
+    
