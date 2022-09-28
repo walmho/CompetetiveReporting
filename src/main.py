2179f17eacd26b5d982b25a7caf90f7be281a8c3
Ryan Jones <jones.ryan.t.01@gmail.com>
2022-09-28 15:06:55 -0700

    Scraper now pulls house rating (out of 5) as well as price per night

--- src/main.py
+++ src/main.py
@@ -1,9 +1,4 @@
 from dataRetrieval.scrapeData import get_page
-import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    status, houseList, rating = get_page("Hillsboro", "Oregon", "US", debug=False)
-    priceVsRating = {houseList[i]: rating[i] for i in range(len(houseList))}
-    
-    plt.bar(rating, houseList)
-    plt.show()
+    status, houseList, rating = get_page("Hillsboro", "Oregon", "US", debug=False) 
