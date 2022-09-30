0ef87328e7781c082cc2c2d3f0d8bebd4e7752b1
Ryan Jones <jones.ryan.t.01@gmail.com>
2022-09-30 12:00:08 -0700

    implementing rating into basic Jinja2 template. Not finished yet, returning unknown error

--- src/createReport/generateTemplate.py
+++ src/createReport/generateTemplate.py
@@ -1,8 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
 
-def createBase(txtFile, index, value):
+def createBase(txtFile, index, value, rating):
     file_loader = FileSystemLoader('templates')
     env = Environment(loader=file_loader)
     template = env.get_template(txtFile)
-    out = template.render(number=index, value=value)
+    out = template.render(number=index, value=value, star=rating)
     return out
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from dataRetrieval.scrapeData import get_page
 from createReport.generateTemplate import createBase
 
 if __name__ == "__main__":
-    status, houseList = get_page("Hillsboro", "Oregon", "US", debug=False)
+    status, houseList, rating = get_page("Hillsboro", "Oregon", "US", debug=False)
     for index in range(len(houseList)):
-        basic = createBase("basics.txt", index, houseList[index])
+        basic = createBase("basics.txt", index, houseList[index], rating)
         print(basic)
