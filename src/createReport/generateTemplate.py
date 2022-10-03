a284a61b6234570e89eb3149110df3b33912ff45
Ryan Jones <jones.ryan.t.01@gmail.com>
2022-10-03 14:51:32 -0700

    turned basic txtTemplate into a class

--- src/createReport/generateTemplate.py
+++ src/createReport/generateTemplate.py
@@ -1,19 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 
-#Should turn this into a class
-def createBase(txtFile, index, value, rating):
-    file_loader = FileSystemLoader('templates')
-    env = Environment(loader=file_loader)
-    template = env.get_template(txtFile)
-    out = template.render(number=index, price=value, star=rating)
-    return out
+class txtTemplate():
+    def createBase(txtFile, index, value, rating):
+        file_loader = FileSystemLoader('templates')
+        env = Environment(loader=file_loader)
+        template = env.get_template(txtFile)
+        out = template.render(number=index, price=value, star=rating)
+        return out
 
-def printOut(file, line):
-    with open(file, 'a+') as f:
-        # Clears any current report. May want to make an archive folder instead and move any previous content there
-        f.write(f"{line}\n")
+    def printOut(file, data):
+        with open(file, 'a+') as f:
+            # Clears any current report. May want to make an archive folder instead and move any previous content there
+            f.write(f"{data}\n")
 
-def dateReport(file):
-    with open(file, 'w') as f:
-        f.write(f"Report generated at {datetime.now()}\n")
+    def dateReport(file):
+        with open(file, 'w') as f:
+            f.write(f"Report generated at {datetime.now()}\n")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from dataRetrieval.scrapeData import get_page
-from createReport.generateTemplate import createBase, printOut, dateReport
+from createReport.generateTemplate import txtTemplate
 
 if __name__ == "__main__":
     status, houseList, ratings = get_page("Hillsboro", "Oregon", "US", debug=False)
 
-    dateReport("templates/output.txt")
+    txtTemplate.dateReport("templates/output.txt")
     for index in range(len(houseList)):
-        basic = createBase("basics.txt", index+1, houseList[index], ratings[index])
-        printOut("templates/output.txt", basic)
+        basic = txtTemplate.createBase("basics.txt", index+1, houseList[index], ratings[index])
+        txtTemplate.printOut("templates/output.txt", basic)
     
