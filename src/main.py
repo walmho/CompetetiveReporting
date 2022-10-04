0955be60431f9edb2f4870a78d6009d8f2dfcb9d
Ryan Jones <jones.ryan.t.01@gmail.com>
2022-10-04 13:26:21 -0700

    added an outputs folder for future reports and .pdf files to be generated / added

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from dataRetrieval.scrapeData import get_page
 from createReport.generateTemplate import txtTemplate
 
 if __name__ == "__main__":
-    status, houseList, ratings = get_page("Hillsboro", "Oregon", "US", debug=False)
+    outFile = "outputs/output.txt"
+    #ppN = price per night
+    status, ppN, ratings = get_page("Hillsboro", "Oregon", "US", debug=False)
+    txtTemplate.dateReport(outFile)
 
-    txtTemplate.dateReport("templates/output.txt")
-    for index in range(len(houseList)):
-        basic = txtTemplate.createBase("basics.txt", index+1, houseList[index], ratings[index])
-        txtTemplate.printOut("templates/output.txt", basic)
+    for index in range(len(ppN)):
+        basic = txtTemplate.createBase("basics.txt", index+1, ppN[index], ratings[index])
+        txtTemplate.printOut(outFile, basic)
     
