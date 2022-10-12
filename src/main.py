b24d8efe347236b284ec0daa8ad550d23388ce82
Ryan Jones <jones.ryan.t.01@gmail.com>
2022-10-12 14:54:56 -0700

    created simple html template to try implementing into current project

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from dataRetrieval.scrapeData import get_page
-from createReport.generateTemplate import txtTemplate
+from createReport.generateTemplate import txtTemplate, pdfTemplate
 
 if __name__ == "__main__":
     outFile = "outputs/output.txt"
-    #ppN = price per night
+    templateFile = "templates/basicLayout.html"
     status, ppN, ratings = get_page("Hillsboro", "Oregon", "US", debug=False)
     txtTemplate.dateReport(outFile)
 
-    for index in range(len(ppN)):
-        basic = txtTemplate.createBase("basics.txt", index+1, ppN[index], ratings[index])
-        txtTemplate.printOut(outFile, basic)
+    pdfTemplate.loadTemplate(templateFile, 0, ppN, ratings)
+
+    # for index in range(len(ppN)):
+    #     basic = txtTemplate.createBase("basics.txt", index+1, ppN[index], ratings[index])
+    #     txtTemplate.printOut(outFile, basic)
